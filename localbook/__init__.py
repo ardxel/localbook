5c9cbea0013a5cfa76a3f6b7dfe606b68d0189e8
ardxel <ardxel@gmail.com>
2025-03-19 20:02:23 +0300

    refactor(LocalBook): refactor. Create static dependencies checker

--- localbook/__init__.py
+++ localbook/__init__.py
@@ -6,6 +6,9 @@
 # @Repository: https://github.com/ardxel/localbook.git
 # ================================================================
 
+import logging
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -18,7 +21,16 @@ from localbook.router import router
 class LocalBook:
     def __init__(self) -> None:
         self.app = FastAPI()
-        port = Deps().get_settings().server.port
+        self._dependencies = Deps()
+
+        self.app.add_event_handler("startup", self._check_static)
+        self.cors()
+        self.static()
+        self.configure_exceptions()
+        self.configure_routes()
+
+    def cors(self):
+        port = self._dependencies.get_settings().server.port
         origins = [
             f"http://localhost:{port}",
             f"http://0.0.0.0:{port}",
@@ -34,6 +46,7 @@ class LocalBook:
             allow_headers=["*"],
         )
 
+    def static(self):
         """STATIC"""
         self.app.mount(
             "/static",
@@ -41,6 +54,7 @@ class LocalBook:
             name="static",
         )
 
+    def configure_exceptions(self):
         """EXCEPTIONS"""
         for exc_handler in exception_handlers:
             self.app.add_exception_handler(
@@ -48,5 +62,20 @@ class LocalBook:
                 handler=exc_handler.handler,
             )
 
+    def configure_routes(self):
         """ROUTING"""
         self.app.include_router(router)
+
+    def _check_static(self):
+        logger = logging.getLogger("localbook")
+
+        if not os.path.exists("static/books"):
+            logger.warning(
+                "User data is not exists in static. Please use special script"
+            )
+
+        if not os.path.exists("static/books/jpeg"):
+            logger.warning("PDF images is not generated. Plese use special script")
+
+        if not os.path.exists("static/modules/pdfjs"):
+            logger.warning("pdfjs module is not exists. Please use special script")
