58b54f76efa60d02cea2b3c3b112f4551087535a
ardxel <ardxel@gmail.com>
2025-04-11 00:17:18 +0300

    🐛 fix(exceptions/handler): prevent app env initialization during tests
    Refactored error rendering to defer dependency initialization.
    Moved Deps() call into `render_error()` to avoid executing app setup in test environment.

--- localbook/exceptions/handlers.py
+++ localbook/exceptions/handlers.py
@@ -2,7 +2,7 @@
 # @Project: LocalBook
 # @Author: Vasily Bobnev (@ardxel)
 # @License: MIT License
-# @Date: 26.03.2025 10:53
+# @Date: 03.04.2025 12:19
 # @Repository: https://github.com/ardxel/localbook.git
 # ================================================================
 
@@ -14,22 +14,22 @@ from localbook.templates import TemplateMap
 
 from .exceptions import BadRequestExpection, UnsupportedMediaTypeException
 
-tmpl = Deps().get_tmpl()
 tmplmap = TemplateMap()
 
 
+def render_error(request: Request, name: str, context={}, _deps: Deps = Deps()):
+    return _deps.tmpl.TemplateResponse(request, name, context=context)
+
+
 async def not_found_handler(request: Request, exc: HTTPException):
-    return tmpl.TemplateResponse(
-        request,
-        name=tmplmap.err404,
-    )
+    return render_error(request, name=tmplmap.err404)
 
 
 async def unsupported_media_type_handler(
     request: Request,
     exc: UnsupportedMediaTypeException,
 ):
-    return tmpl.TemplateResponse(
+    return render_error(
         request,
         name=tmplmap.err415,
         context={"bad_mime": exc.mime or "unknown"},
@@ -38,7 +38,4 @@ async def unsupported_media_type_handler(
 
 async def bad_request_handler(request: Request, exc: BadRequestExpection):
     # TODO: create specific template for handling bad request error
-    return tmpl.TemplateResponse(
-        request,
-        name=tmplmap.err404,
-    )
+    return render_error(request, name=tmplmap.err404)
