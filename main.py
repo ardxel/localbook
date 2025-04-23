b2a0fd39c67fd090623d449267d58e6c80ce6b06
ardxel <ardxel@gmail.com>
2025-04-23 15:14:31 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+# ================================================================
+# @Project: LocalBook
+# @Author: Vasily Bobnev (@ardxel)
+# @License: MIT License
+# @Date: 19.04.2025 16:27
+# @Repository: https://github.com/ardxel/localbook.git
+# ================================================================
+
 from localbook import LocalBook
 
 localbook = LocalBook()
 app = localbook.app
+# if __name__ == "__main__":
+#     localbook = LocalBook()
+#     app = localbook.app
